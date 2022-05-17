25e5894a411b486b9248f818a5c35d244093b567
wahidnadeem <wahidnadeem366@gmal.com>
2022-05-17 09:09:54 +0430

    sowe work in pdf report

--- tcpdf/report_customer_pdf.php
+++ tcpdf/report_customer_pdf.php
@@ -32,78 +32,203 @@ $pdf->SetHeaderData(    PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH,"" );
 
 $pdf->AddPage();
 
-$main_data = '
 
+$condition  = VD($_GET['query']);
+
+$TIME_STAMP       = date('Y-m-d h-m-s');
+$TIME_STAMP       = str_replace(' ','__',$TIME_STAMP);
+$DATE       = date('Y-m-d');
+$PDATE      = clean_data(gregorian_to_jalali_date($DATE,'-'));
+
+
+
+ $view_data = $db->query("SELECT * FROM `accounts` WHERE `deleted` = '0' $condition ORDER BY id DESC ");
+$add_br  = '';
+$number_loop_count = $view_data->rowCount() % 20;
+
+if($number_loop_count > 11 ){
+    $add_br = '<br><br><br><br><br><br><br><br><br><br>';
+}
+
+$main_data = '';
+$count = 1;
+if($view_data->rowCount() > 0  ){                          
+                                            
+    foreach ($view_data as $key => $row) {
+
+        $array_temp     = [1=> 0,2=>0,3=>0,4=>0,5=>0];
+        $total_balance_doller  = 0;
+        $account_id = $row['id'];
+
+
+        $account_balance_data = $db->query("SELECT amount, currency_id FROM `account_balance` WHERE `account_id`='$account_id' $currency_condition  ");
+     
+        foreach ($account_balance_data as $key => $account_balance_row) {
+           $array_temp[$account_balance_row['currency_id']] = $account_balance_row['amount'];                                
+        }
+
+        foreach ($array_temp as $key => $array_temp_row) {
+            
+            if($array_temp_row == 0 )
+                continue;
+
+            $currency_id = $key;
+            
+            $currency_rate  = $db->query("SELECT `sell_price` , `amount_transaction` FROM `currency_rate` WHERE currency_from_id = '2' AND currency_to_id = '$currency_id'  ");
+            $currency_rate_value    = 1;
+            $amount_transaction     = 1;
+            
+            if($currency_rate->rowCount() > 0 ){
+                
+                $currency_rate_row      = $currency_rate->fetch();
+                $currency_rate_value    = ($currency_rate_row['sell_price'] == 0 ) ? 1 : $currency_rate_row['sell_price'] ;
+                $amount_transaction     = ($currency_rate_row['amount_transaction'] == 0) ? 1 : $currency_rate_row['amount_transaction'];
+                
+            } 
+
+            if($currency_id == 2){
+                $temp = $array_temp_row;
+            }else {
+                $temp = $array_temp_row / ( $currency_rate_value * $amount_transaction );                
+            }
+
+            $total_balance_doller += $temp;
+        }
+
+
+        
+        echo '
+            <tr> 
+                <td> '.($count++).'</td>
+                <td> '.$row['account_code'].' </td>
+                <td> '.$row['first_name'].' </td>
+                <td> <span class="h4 mb-0 togg priv">******</span><span class="togg CurNumDiv  currSign" dir="ltr" > '.$array_temp[1].' </span> </td>
+                <td> <span class="h4 mb-0 togg priv">******</span><span class="togg CurNumDiv  currSign" dir="ltr" > '.$array_temp[2].' </span> </td>
+                <td> <span class="h4 mb-0 togg priv">******</span><span class="togg CurNumDiv  currSign" dir="ltr" > '.$array_temp[3].' </span> </td>
+                <td> <span class="h4 mb-0 togg priv">******</span><span class="togg CurNumDiv  currSign" dir="ltr" > '.$array_temp[4].' </span> </td>
+                <td> <span class="h4 mb-0 togg priv">******</span><span class="togg CurNumDiv  currSign" dir="ltr" > '.$array_temp[5].' </span> </td>
+                <td> <span class="h4 mb-0 togg priv">******</span><span class="togg CurNumDiv  currSign" dir="ltr" >  '.$total_balance_doller.'</span> </td>
+            </tr>
+
+        ';
+    }
+
+}
+ else {
+    $main_data .= '
     <tr>
-        <td align="center">100000</td>
-        <td align="center">12133</td>
-        <td align="center">1222333</td>
-        <td align="center" style="color:red">12222</td>
-        <td align="center">    12</td>
-        <td align="center"> 2,532</td>
-        <td align="center"> 211</td>
-        <td align="center">12</td>
-        <td align="center">1</td>
+    <td colspan="7" class="text-center" style="text-align:center">
+    <br>
+    کدام اطلاعاتی وجود ندارد 
+    <br><br>
+    </td>
     </tr>
-     <tr>
-        <td align="center">20000000</td>
-        <td align="center">12133</td>
-        <td align="center">1222333</td>
-        <td align="center" style="color:red">12222</td>
-        <td align="center">    12</td>
-        <td align="center"> 2,532</td>
-        <td align="center"> 211</td>
-        <td align="center">12</td>
-        <td align="center">1</td>
-    </tr>
-    
+    ';
+ }
+ $totla_amount = '';
 
-';
+         $array_temp     = [1=> 0,2=>0,3=>0,4=>0,5=>0];
+         $temp           = 0;
 
-$totla_amount='
-     <tr>
-        <td align="center">10000000</td>
-        <td align="center">10000000</td>
-        <td align="center">10000000</td>
-        <td align="center">10000000</td>
-        <td align="center">10000000</td>
-        <td align="center">10000000</td>
-    </tr>
-';
+         if(empty($_POST['search'])){
+            
+            $total_balance = $db->query("SELECT SUM(amount) as amount , currency_id FROM `account_balance` $currency_condition  GROUP BY currency_id ");
+
+            foreach ($total_balance as $key => $row) {
+               $array_temp[$row['currency_id']] = $row['amount'];
+            }
+
+         }else {
+
+            $view_data = $db->query(" SELECT * FROM `accounts` WHERE `deleted` = '0' $condition ");
+
+            foreach ($view_data as $key => $accounts_row ) {
+               $account_id = $accounts_row['id'];
+               $total_balance_customer = $db->query("SELECT SUM(amount) as amount , currency_id FROM `account_balance` WHERE account_id = $account_id $currency_condition  GROUP BY currency_id ");
+
+               foreach ($total_balance_customer as $key => $account_balance_row) {
+                  $array_temp[$currency_id] += $account_balance_row['amount'];
+               }
+            }
+
+         }
+
+         $temp           = 0;
+         $total_balance_doller = 0;
+         foreach ($array_temp as $key => $array_temp_row) {
+                
+            if($array_temp_row == 0 )
+               continue;
+
+            $currency_id = $key;
+            
+            $currency_rate  = $db->query("SELECT `sell_price` , `amount_transaction` FROM `currency_rate` WHERE currency_from_id = '2' AND currency_to_id = '$currency_id'  ");
+            $currency_rate_value    = 1;
+            $amount_transaction     = 1;
+            
+            if($currency_rate->rowCount() > 0 ){
+                
+               $currency_rate_row      = $currency_rate->fetch();
+               $currency_rate_value    = ($currency_rate_row['sell_price'] == 0 ) ? 1 : $currency_rate_row['sell_price'] ;
+               $amount_transaction     = ($currency_rate_row['amount_transaction'] == 0) ? 1 : $currency_rate_row['amount_transaction'];
+                
+            } 
+
+            if($currency_id == 2){
+               $temp = $array_temp_row;
+            }else {
+               $temp = $array_temp_row / ( $currency_rate_value * $amount_transaction );                
+            }
+
+            $total_balance_doller += $temp;
+         }
+
+         echo '
+            <tr>
+               <th><span class="h4 mb-0 togg priv">******</span><span class="togg CurNumDiv  currSign" dir="ltr" > '.$array_temp[1].'  </span></th>
+               <th><span class="h4 mb-0 togg priv">******</span><span class="togg CurNumDiv  currSign" dir="ltr" > '.$array_temp[2].'  </span></th>
+               <th><span class="h4 mb-0 togg priv">******</span><span class="togg CurNumDiv  currSign" dir="ltr" > '.$array_temp[3].'  </span></th>
+               <th><span class="h4 mb-0 togg priv">******</span><span class="togg CurNumDiv  currSign" dir="ltr" > '.$array_temp[4].'  </span></th>
+               <th><span class="h4 mb-0 togg priv">******</span><span class="togg CurNumDiv  currSign" dir="ltr" > '.$array_temp[5].'  </span></th>
+               <th><span class="h4 mb-0 togg priv">******</span><span class="togg CurNumDiv  currSign" dir="ltr" > '.$total_balance_doller.'  </span></th>
+            </tr>
+         ';
+
 
 $html = '
-    <div style ="text-align:right;position:relative;top:30px; font-size:16px  " >گزارش   بیلانس عمومی مشتریان</div>
-    
-    <div style ="text-align:right;position:relative;top:30px; font-size:16px  " >تاریخ گزارش  :  1400/12/2</div>
+    <div style ="text-align:right;position:relative;top:30px; font-size:16px  " >   گزارش انتقالات و معاملات پولی </div>
+    <div style ="text-align:right;position:relative;top:30px; font-size:16px  " >   دارنده حساب :   </div>
+    <div style ="text-align:right;position:relative;top:30px; font-size:16px  " > شماره حساب  : </div>
+    <div style ="text-align:right;position:relative;top:30px; font-size:16px  " >تاریخ گزارش  : '.$PDATE.' </div>
     <br>
 
 <table border="1"  cellpadding="5">
     <tr>
-        <th align="center" style="font-weight:600;background-color:#95ccec;color:#000"  width="100">بیلانس</th>
-        <th align="center" style="font-weight:600;background-color:#95ccec;color:#000"  width="75">یورو</th>
-        <th align="center" style="font-weight:600;background-color:#95ccec;color:#000"  width="75">کلدار</th>
-        <th align="center" style="font-weight:600;background-color:#95ccec;color:#000" width="75">تومان</th>
-        <th align="center" style="font-weight:600;background-color:#95ccec;color:#000"  width="75"> دالر</th>
-        <th align="center" style="font-weight:600;background-color:#95ccec;color:#000"  width="75"> افغانی</th>
-        <th align="center" style="font-weight:600;background-color:#95ccec;color:#000"  width="75">نام</th>
-        <th align="center" style="font-weight:600;background-color:#95ccec;color:#000"  width="75">حساب</th>
-        <th align="center" style="font-weight:600;background-color:#95ccec;color:#000"  width="40" >#</th>
+        <th  align="center" style="font-weight:600;background-color:#95ccec;color:#000"  width="150">شماره</th>
+        <th  align="center" style="font-weight:600;background-color:#95ccec;color:#000"  width="150">حساب</th>
+        <th  align="center" style="font-weight:600;background-color:#95ccec;color:#000"  width="150">نام</th>
+        <th  align="center" style="font-weight:600;background-color:#95ccec;color:#000"  width="150">افغانی</th>
+        <th  align="center" style="font-weight:600;background-color:#95ccec;color:#000"  width="150">دالر</th>
+        <th  align="center" style="font-weight:600;background-color:#95ccec;color:#000"  width="150">تومان</th>
+        <th  align="center" style="font-weight:600;background-color:#95ccec;color:#000"  width="150">کلدار</th>
+        <th  align="center" style="font-weight:600;background-color:#95ccec;color:#000"  width="150">یورو</th>
+        <th  align="center" style="font-weight:600;background-color:#95ccec;color:#000"  width="150">بیلانس</th>
     </tr>
     '.$main_data.'
    
 </table>
 <br><br><br>'.$add_br.'
 <h4 align="right">
- مجموع  مفاد
- </h4>
+ مجموع کل
+</h4>
 <table border="1"  cellpadding="4">
     <tr>
-        <th align="center">  به دالر</th>
-        <th align="center"> یورو</th>
-        <th align="center"> کلدار</th>
-        <th align="center"> تومان</th>
-        <th align="center">  دالر</th>
-        <th align="center"> افغانی</th>
+         <th align="center"> مجموعه افغانی   </th>
+         <th align="center"> مجموعه دالر </th>
+         <th align="center"> مجموعه تومان </th>
+         <th align="center">مجموعه  کلدار</th>
+         <th align="center">مجموعه یورو</th>
+         <th align="center"> مجموعه به دالر  </th>
     </tr>
     
     '.$totla_amount.'
