fc6c7c3862458f5b6a830ed057b8c67c3f905d5a
wahidnadeem <wahidnadeem366@gmail.com>
2022-12-20 00:03:32 +0430

    report buy and sell is done and it is OK !

--- view/report_buy_sell.php
+++ view/report_buy_sell.php
@@ -238,7 +238,7 @@ require_once("_config.php");
                      <div class="card">
                         <div class="card-body">
                            <div class="row mt-2">
-                              <h5 class="text-start lalezar text-muted mb-5 ms-3">گذارشات عمومی</h5>
+                              <h5 class="text-start lalezar text-muted mb-5 ms-3">  گذارشات عمومی <span> تاریخ <?php echo persionData($PDATE)?></span></h5>
                               <div class="row mt-2">
                                  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
                                     <table id="datatable" class="table table-striped dt-responsive nowrap w-100">
